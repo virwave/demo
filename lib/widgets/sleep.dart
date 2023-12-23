e33c0da191bde239bc33441d4d221abdfc661c60
Kate Julia <kate@virwave.com>
2023-12-23 14:20:24 -0800

    bed placement dragging

--- lib/widgets/sleep.dart
+++ lib/widgets/sleep.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'animals_data.dart';
-import 'common_buttons.dart';
-import 'common_buttons_animals.dart';
+import 'animals_data.dart'; // Replace with your animal data import
+import 'common_buttons.dart'; // Replace with your common buttons import
+import 'common_buttons_animals.dart'; // Replace with your common animal buttons import
 
 class SleepPage extends StatefulWidget {
   final String animalName;
@@ -13,12 +13,17 @@ class SleepPage extends StatefulWidget {
 }
 
 class _SleepPageState extends State<SleepPage> {
-  bool isAnimalOnBed = false;
+  late AnimalData? _animalData;
+  bool _isOnBed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animalData = findAnimalData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final AnimalData? animalData = findAnimalData();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sleep Page'),
@@ -26,55 +31,75 @@ class _SleepPageState extends State<SleepPage> {
       body: Center(
         child: Stack(
           children: [
+            // Bed Image (DragTarget)
             Positioned(
-              top: MediaQuery.of(context).size.height * 0.45,
-              right: 10,
-              child: DragTarget<String>(
-                builder: (BuildContext context, List<String?> candidateData,
-                    List<dynamic> rejectedData) {
+              top: MediaQuery.of(context).size.height * 0.4,
+              left: MediaQuery.of(context).size.width * 0.4,
+              child: DragTarget<AnimalData>(
+                builder: (context, candidateAnimals, rejectedAnimals) {
                   return Image.asset(
                     'assets/images/icons/smile.png', // Replace with your bed image path
                     width: 100,
                     height: 100,
                   );
                 },
-                onWillAccept: (data) => data == 'Animal',
-                onAccept: (data) {
+                onWillAccept: (data) =>
+                    true, // Allow dropping the animal on the bed
+                onAccept: (animal) {
                   setState(() {
-                    isAnimalOnBed = true;
+                    _isOnBed = true;
                   });
                 },
               ),
             ),
+            // Animal Image (Draggable)
             Positioned(
-              top: MediaQuery.of(context).size.height * 0.4,
-              left: 10,
-              child: Draggable<String>(
-                data: 'Animal',
-                child: isAnimalOnBed
-                    ? SizedBox(
-                        width: 100,
-                        height: 100,
+              left: _isOnBed
+                  ? MediaQuery.of(context).size.width * 0.5
+                  : MediaQuery.of(context).size.width * 0.2,
+              top: _isOnBed
+                  ? MediaQuery.of(context).size.height * 0.5
+                  : MediaQuery.of(context).size.height * 0.2,
+              child: Draggable<AnimalData>(
+                data: _animalData!,
+                child: _isOnBed
+                    ? Opacity(
+                        opacity: 0.0,
                         child: Image.asset(
-                          animalData!.sleepImagePath,
+                          _animalData!.imagePath,
+                          width: 100,
+                          height: 100,
                           fit: BoxFit.contain,
                         ),
                       )
                     : Image.asset(
-                        animalData!.sleepImagePath,
+                        _animalData!.imagePath, // Regular image of the animal
                         width: 100,
                         height: 100,
                         fit: BoxFit.contain,
                       ),
                 feedback: Image.asset(
-                  animalData.sleepImagePath,
+                  _animalData!
+                      .imagePath, // Regular image as feedback when dragging
                   width: 100,
                   height: 100,
                   fit: BoxFit.contain,
                 ),
                 childWhenDragging: Container(),
+                onDraggableCanceled: (_, __) {},
               ),
             ),
+            // Sleeping Animal Image (Visible when on the bed)
+            if (_isOnBed)
+              Center(
+                child: Image.asset(
+                  _animalData!
+                      .sleepImagePath, // Show sleeping image when the animal is on the bed
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.contain,
+                ),
+              ),
           ],
         ),
       ),
